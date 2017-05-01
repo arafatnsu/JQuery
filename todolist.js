6a629ec0332cb3122336a7c5e54944f31c12067d
arafatnsu <arafatnsu@yahoo.com>
2017-05-01 06:58:18 -0400

    comments and minor changes

--- todolist.js
+++ todolist.js
@@ -1,23 +1,41 @@
-function addListItem() {
-  $('#add_task').on('click', function() {
-    var text = $('#to_do').val();
+// validating task on basis of user input, throw error if field is empty
+function addItem() {
+  var text = $('#to_do').val();
     if(text === '') {
       alert('name is required');
     } else {
       $('#list_item').append('<li><input class="completeItem" type="checkbox">'+text+'<i class="fa fa-trash-o deleteItem" aria-hidden="true"></li>');
       $('#to_do').val('');
     }
-  });
 }
 
+function addListItem() {
+  /*
+    add task while clicking button
+  */
+  $('#add_task').on('click', function() {
+    addItem()
+  });
+  
+  // add task while pressing enter key
+  $('#to_do').on('keypress', function(event) {
+        if(event.which === 13) {
+            addItem();
+            event.preventDefault();
+        }
+    });
+}
+
+//jquery function for dynamic elements
 function completeItem() {
-    $('.completeItem').on('click', function() {
+    $(document).on('click', '.completeItem', function() {
       $(this).parent().toggleClass('done')
     })
 }
 
+// functions executed for deleting task
 function deleteItem() {
-  $('.deleteItem').on('click', function() {
+  $(document).on('click', '.deleteItem', function() {
     if($(this).parent().find('.completeItem').is(':checked')) {
           $(this).parent().remove();
       }
@@ -28,18 +46,9 @@ function deleteItem() {
     
 }
 
-function checkForListItem() {
-  $('#to_do').on('keypress', function(event) {
-        if(event.which === 13) {
-            addListItem();
-            event.preventDefault();
-        }
-    });
-}
-
+// functions executed while loading page
 $(document).ready(function(){
     addListItem()
     deleteItem()
     completeItem()
-    checkForListItem()
 });
